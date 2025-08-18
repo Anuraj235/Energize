f21cae699e8716d6dd5ac188d31e9e79e67138a5
Anuraj235 <pantanuraj31@gmail.com>
2025-08-18 16:52:39 -0500

    made routes for home and gallery

---
+++ client/energize-web/src/pages/Gallery.tsx
@@ -0,0 +1,8 @@
+export default function Gallery() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 28 }}>Gallery</h1>
+      <p>Simple gallery page.</p>
+    </section>
+  );
+}
---
+++ client/energize-web/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 28 }}>Home</h1>
+      <p>Welcome to Energize ⚡</p>
+    </section>
+  );
+}
